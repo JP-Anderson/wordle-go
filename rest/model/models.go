5dbd93ce000ce64c5acc310c2a6b892da2170fe6
JP Anderson <dahui58@hotmail.com>
2023-03-08 18:32:46 +0700

    add model classes for endpoint JSON responses

---
+++ rest/model/models.go
@@ -0,0 +1,26 @@
+package models
+
+// Guess represents a guess which has been made in the past.
+// - GuessWord is the actual guess made by the player.
+// - LetterStatuses is a string the same length as the guess word, each index contains a number indicating the correctness of the same index in the guess.
+type Guess struct {
+	GuessWord string `json:"guess_word"`
+	LetterStatuses string `json:"letter_statuses"`
+}
+
+// Game represents a game of Wordle which may be complete or in progress.
+// This will be returned from the GET/game and POST/guess endpoints.
+type Game struct {
+	GameID string `json:"game_id"`
+	UserID string `json:"user_id"`
+	Guesses []Guess `json:"guesses"`
+	TotalGuesses int `json:"total_guesses"`
+	GameState int `json:"game_state"`
+}
+
+// Answer contains the answer for a selected game.
+// This has its own endpoint because we do not return the Answer in the regular Game state structure.
+// This will only be returned for completed (won or lost) games.
+type Answer struct {
+	Answer string `json:"answer"`
+}
