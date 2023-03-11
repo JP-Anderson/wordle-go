aa7f8c9b2d9ebfe9f020c0e00f32242355abea40
JP Anderson <dahui58@hotmail.com>
2023-03-11 21:41:18 +0700

    letter td css classes now set from model

--- ui/wordle-react/src/WordleRow.js
+++ ui/wordle-react/src/WordleRow.js
@@ -3,9 +3,11 @@ export default function WordleRow(props) {
 	  <tr>
 	    {Array.from({ length: props.targetLength }, (_, i) => {
 	      if ( !props.guess ) return <td key={i}></td>;
-	      else return <td class="correct letter" key={i}>{props.guess[0][i]}</td>
+	      else return <td class={statusIntToClasses[props.guess[1][i]]} key={i}>{props.guess[0][i]}</td>
 	    })
 	    }
 	  </tr>
 	);
 }
+
+const statusIntToClasses = { "2" : "correct letter", "1" : "misplaced letter", "0" : "incorrect letter" };
