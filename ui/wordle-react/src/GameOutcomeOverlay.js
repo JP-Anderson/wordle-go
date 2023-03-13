a724902e9205dc5a0eaf857b1ca1b0440beef0f6
JP Anderson <dahui58@hotmail.com>
2023-03-13 14:47:32 +0700

    display answer if the user loses

--- ui/wordle-react/src/GameOutcomeOverlay.js
+++ ui/wordle-react/src/GameOutcomeOverlay.js
@@ -1,7 +1,8 @@
 export default function GameOutcomeOverlay(props) {
 	console.log(props);
 	if (props.isOpen) {
-		const message = props.data.game_state == 2 ? "You won!" : "You lost!";
+		const target = props.data.answer.answer;
+		const message = props.data.game_state == 2 ? "You won!" : "You lost! The answer was "+target;
  		return (
 		  <div id="outcomeModal" className="modal">
 		    <div className="modal-content">
